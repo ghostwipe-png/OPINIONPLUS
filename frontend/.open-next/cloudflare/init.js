b8c21a17a7fed7be7c3645c53e12cca3753f259a
ghostwipe-png <petersonotila@gmail.com>
2026-07-17 14:30:42 +0300

    Add Paystack payment integration

--- frontend/.open-next/cloudflare/init.js
+++ frontend/.open-next/cloudflare/init.js
@@ -47,7 +47,7 @@ function initRuntime() {
   };
   Object.assign(globalThis, {
     Request: CustomRequest,
-    __BUILD_TIMESTAMP_MS__: 1784284596068,
+    __BUILD_TIMESTAMP_MS__: 1784287711790,
     __NEXT_BASE_PATH__: "",
     __ASSETS_RUN_WORKER_FIRST__: false,
     __TRAILING_SLASH__: false,
